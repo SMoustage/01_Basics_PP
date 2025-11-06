7bc3095d5febacfc9bb15c4bf07f80a96104a82e
Florian Holz <stathi.koko@gmail.com>
2025-11-06 10:14:30 +0000

    Erste Variabeln

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -7,3 +7,18 @@
  */
 
 /***** Variablen 01 *******/
+
+// console.log("Hi");
+// console.log(12);
+
+/* Deklaration + Wertzuweisung I */
+
+
+
+let firstName;  // Deklaration (Definition)
+console.log(firstName); // Ausgabe 
+firstName = "Florian";
+console.log("Inhalt: " +firstName); // Ausgabe 
+let familyName = "Holz"; // Deklaration & Wertzuweisung 
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Hi, ich bin Florian Holz!
+
