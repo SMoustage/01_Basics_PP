2c586e19a61c1aba75dc7c04446afa3b1b14602f
Florian Holz <stathi.koko@gmail.com>
2025-11-07 10:44:13 +0000

    Berechnung Alter Iteration 2

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,13 +3,26 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 1980;
+const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2025;
+// Berechnung Alter Iteration 1 
+// let year = 2025;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration 2
+let date = new Date();
+let year = date.getFullYear();
+
+console.log("Datum: " + date);
+console.log("Jahr: " + year);
+
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
+
+
+
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
