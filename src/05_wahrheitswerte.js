9e361bf6056c9e4db212c47504b4bb876ce613c1
Florian Holz <stathi.koko@gmail.com>
2025-11-07 11:21:06 +0000

    Deklaration Tests Aussagenlogik

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -4,7 +4,7 @@
 // Deklaration
 let ageJohn, ageMark;
 const birthYearJohn = 2000;
-const birthYearMark = 1990;
+const birthYearMark = 2000;
 
 // Berechnung Alter Iteration 1 
 // let year = 2025;
@@ -12,19 +12,31 @@ const birthYearMark = 1990;
 // ageMark = year - birthYearMark;
 
 // Berechnung Alter Iteration 2
-let date = new Date();
-let year = date.getFullYear();
+// let date = new Date();
+// let year = date.getFullYear();
 
-console.log("Datum: " + date);
-console.log("Jahr: " + year);
+// console.log("Datum: " + date);
+// console.log("Jahr: " + year);
 
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration 3
+
+let year = new Date().getFullYear();
+// console.log("Jahr: " + year);
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
-
-
 
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
+
+// Deklaration Tests Aussagenlogik
+// let isJohnOlder = false;
+let isJohnOlder = (ageJohn > ageMark); // Test auf ungleicheheit 
+let isJohnEqual = (ageJohn == ageMark); // Test auf Gleichheit 
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual); 
