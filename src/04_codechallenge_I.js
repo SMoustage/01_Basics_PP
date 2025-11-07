309c0eecfdbf1d63d336da5e691f24a1ff54d019
Florian Holz <stathi.koko@gmail.com>
2025-11-07 09:52:48 +0000

    Lösungsbeispiele

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -10,3 +10,41 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 const prompt = require('prompt-sync')({sigint: true});
+
+
+// Gute Lesbarkeit
+
+// let ziffer1, ziffer2, zahl1, zahl2, summe;
+
+// ziffer1 = prompt("Bitte Zahl 1 eingeben ");
+// ziffer2 = prompt("Bitte Zahl 2 eingeben ");
+
+// console.log("Datentyp: " + typeof ziffer1);
+// console.log("Datentyp: " + typeof ziffer2);
+
+// zahl1 = parseFloat(ziffer1);
+// zahl2 = parseFloat(ziffer2);
+
+// console.log("Datentyp: " + typeof zahl1);
+// console.log("Datentyp: " + typeof zahl2);
+
+// summe = zahl1 + zahl2;
+
+// console.log("Die Summe der Zahl ist: " +summe);
+
+// ----------------------------------------------------
+
+// Kompakte Lösung 
+
+// let zahl1, zahl2, summe;
+
+// zahl1 = parseInt(prompt("Zahl 1: "));
+// zahl2 = parseInt(prompt("Zahl 2: "));
+
+// summe = zahl1 + zahl2;
+
+// console.log("Die Summe der Zahl ist: " +summe);
+
+
+
+
